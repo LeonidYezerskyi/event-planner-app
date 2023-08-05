c72f9a00ea34ab3f99a5cd9a150dc8029c04bab1
LeonidYezerskyi <111503058+LeonidYezerskyi@users.noreply.github.com>
2023-08-05 16:13:43 +0300

    ADDED: markup of sortBy filter

--- src/components/Main/CategorySearch/CategorySearch.jsx
+++ src/components/Main/CategorySearch/CategorySearch.jsx
@@ -1,11 +1,35 @@
+import { useState } from "react"
 import { CiFilter } from 'react-icons/ci'
+import cn from 'classnames';
+
 import css from "./CategorySearch.module.css"
 
 const CategorySearch = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
-        <div className={css.categoryWrapper}>
-            <CiFilter className={css.icon} />
-        </div>
+        <>
+            <div className={css.categoryWrapper} onClick={() => setIsOpen(!isOpen)}>
+                <CiFilter className={css.icon} />
+            </div>
+            {
+                isOpen && (
+                    <div className={css.openWrapper} onClick={() => setIsOpen(!isOpen)}>
+                        <div className={css.mainField} >
+                            <CiFilter className={cn(css.icon, css.icon2)} />
+                            <span className={css.mainFieldText}>Category</span>
+                        </div>
+                        <p>Art</p>
+                        <p>Music</p>
+                        <p>Business</p>
+                        <p>Conference</p>
+                        <p>Workshop</p>
+                        <p>Party</p>
+                        <p className={css.optionText}>Sport</p>
+                    </div>
+                )
+            }
+        </>
     )
 }
 
--- src/components/Main/SortBy/SortBy.jsx
+++ src/components/Main/SortBy/SortBy.jsx
@@ -1,11 +1,53 @@
+import { useState } from "react"
+import { GoArrowUp, GoArrowDown } from "react-icons/go"
 import css from './SortBy.module.css'
 import sort from "../../../assets/sort.svg"
+import sort2 from "../../../assets/sort2.svg"
+
 
 const SortBy = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
-        <div className={css.sortWrapper}>
-            <img className={css.img} src={sort} alt="sort-icon" />
-        </div>
+        <>
+            <div className={css.sortWrapper} onClick={() => setIsOpen(!isOpen)}>
+                <img className={css.img} src={sort} alt="sort-icon" />
+            </div>
+            {
+                isOpen && (
+                    <div className={css.openWrapper} onClick={() => setIsOpen(!isOpen)}>
+                        <div className={css.mainField}>
+                            <span className={css.mainFieldText}>Sort by</span>
+                            <img className={css.img} src={sort2} alt="sort-icon" />
+                        </div>
+                        <div className={css.sortItem}>
+                            <p>by name</p>
+                            <GoArrowUp className={css.iconField} />
+                        </div>
+                        <div className={css.sortItem}>
+                            <p>by name</p>
+                            <GoArrowDown className={css.iconField} />
+                        </div>
+                        <div className={css.sortItem}>
+                            <p>by data</p>
+                            <GoArrowUp className={css.iconField} />
+                        </div>
+                        <div className={css.sortItem}>
+                            <p>by data</p>
+                            <GoArrowDown className={css.iconField} />
+                        </div>
+                        <div className={css.sortItem}>
+                            <p>by priority</p>
+                            <GoArrowUp className={css.iconField} />
+                        </div>
+                        <div className={css.sortItem}>
+                            <p>by priority</p>
+                            <GoArrowDown className={css.iconField} />
+                        </div>
+                    </div>
+                )
+            }
+        </>
     )
 }
 
