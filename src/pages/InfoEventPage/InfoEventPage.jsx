0053f5cb2b5ee20d052e2a7011005e1d46f72ddd
LeonidYezerskyi <111503058+LeonidYezerskyi@users.noreply.github.com>
2023-08-06 16:25:48 +0300

    ADDED: adaptive styles for more info page

--- src/pages/InfoEventPage/InfoEventPage.jsx
+++ src/pages/InfoEventPage/InfoEventPage.jsx
@@ -12,21 +12,23 @@ const InfoEventPage = () => {
         <div className={css.infoContainer}>
             <div className={css.infoWrapper}>
                 <GoBackButton />
-                <h2 className={css.infoTitle}>{event.title}</h2>
+                <div className={css.mainInfoWrapper}>
+                    <h2 className={css.infoTitle}>{event.title}</h2>
 
-                <div className={css.cardWrapper}>
-                    <img src={event.image} className={css.img} alt="event picture" />
-                    <div className={css.textWrapper}>
-                        <p className={css.descrText}>{event.description}</p>
-                        <div className={css.infoTextWrapper}>
-                            <span className={css.categoryText}>{event.category}</span>
-                            <span className={css.categoryText}>{event.priority}</span>
-                            <span className={css.categoryText}>{event.place}</span>
-                            <span className={css.categoryText}>{event.date} at {event.time}</span>
-                        </div>
-                        <div className={css.btnWrapper}>
-                            <button type='button' onClick={() => { }} className={css.editBtn}>Edit</button>
-                            <Button text="Delete event" className={css.delBtn} />
+                    <div className={css.cardWrapper}>
+                        <img src={event.image} className={css.img} alt="event picture" />
+                        <div className={css.textWrapper}>
+                            <p className={css.descrText}>{event.description}</p>
+                            <div className={css.infoTextWrapper}>
+                                <span className={css.categoryText}>{event.category}</span>
+                                <span className={css.categoryText}>{event.priority}</span>
+                                <span className={css.categoryText}>{event.place}</span>
+                                <span className={css.categoryText}>{event.date} at {event.time}</span>
+                            </div>
+                            <div className={css.btnWrapper}>
+                                <button type='button' onClick={() => { }} className={css.editBtn}>Edit</button>
+                                <Button text="Delete event" className={css.delBtn} />
+                            </div>
                         </div>
                     </div>
                 </div>
